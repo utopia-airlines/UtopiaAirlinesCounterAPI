4ecd0204fa03cea4316aaad7d4d678b25820bc8a
Al Amine <amine.anaji@gmail.com>
2019-06-24 21:48:49 +0100

    the models

---
+++ src/main/java/com/st/utopia/counter/entity/PaymentAmount.java
@@ -0,0 +1,20 @@
+package com.st.utopia.counter.entity;
+
+/**
+ * A simple wrapper around a single integer representing an amount paid for a
+ * ticket.
+ *
+ * @author Jonathan Lovelace
+ */
+public class PaymentAmount {
+	/**
+	 * The amount paid.
+	 */
+	private int price;
+	/**
+	 * @return the amount paid
+	 */
+	public int getPrice() {
+		return price;
+	}
+}
